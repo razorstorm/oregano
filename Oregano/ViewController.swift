28a83331b736b59e3af9a38a8b7e06193141a1d7
Jason Jiang <jjiang1991@gmail.com>
2018-11-05 19:12:07 -0800

    Initial Commit

---
+++ Oregano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oregano
+//
+//  Created by Jason Jiang on 11/5/18.
+//  Copyright © 2018 Jason Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
